ffe153bbe62e27facc2bf50c80403c807827f804
Kate <katensullivan55@gmail.com>
2022-07-01 01:00:18 -0400

    js constants linking class

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// necesary node info
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// file connections
+const employee = require('./lib/employee.js');
+const manager = require('./lib/manager.js');
+const engineer = require('./lib/engineer.js');
+const intern = require('./lib/intern.js');
+
+
+
 // WHEN I am prompted for my team members and their information
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 
